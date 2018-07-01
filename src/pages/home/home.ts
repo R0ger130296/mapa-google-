d08d2cc0dfe3c27fe45fa374d8b409b8f26264df
Omar Cedeño <grarogccee@gmail.com>
2018-07-01 18:38:29 -0500

    ok

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -19,49 +19,55 @@ export class HomePage implements OnInit {
   ngOnInit() {
  this.derivada();
  this.graficar();
+}
+
+derivada() {
+  this.funcion = 'd(Sin(x))/dx';
+  const h = 1;
+  let fx = 0;
+  for(let i = 0; i<=4 ; i++){
+    this.etiquetas.push(i.toString());
+    fx = Math.round(Math.random());
+    let fxMASh = Math.round(Math.random() + h);
+    let Derivada = (fxMASh - fx)/h;
+    this.valores.push(Derivada);
   }
-  derivada() {
-    this.funcion = 'd(Sin(x))/dx';
-    const h = 1;
-    let fx = 0;
-    for(let i = 0; i<=4 ; i++){
-      this.etiquetas.push(i.toString());
-      fx = Math.round(Math.random());
-      let fxMASh = Math.round(Math.random() + h);
-      let Derivada = (fxMASh - fx)/h;
-      this.valores.push(Derivada);
-    }
-  }
-  graficar() {
-    this.lineChart = new Chart(this.lineCanvas.nativeElement, {
-      type: 'pie',
-      data: {
-        labels: this.etiquetas,
-        datasets: [
-          {
-            label: this.funcion,
-            fill: false,
-            lineTension: 0,
-            backgroundColor: "rgba(0,0,255,0.5)",
-            borderColor: "rgba(0,0,255,0.5)",
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(255,0,0,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(0,255,0,1)",
-            pointHoverBorderColor: "rgba(255,0,0,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 2,
-            pointHitRadius: 10,
-            data: this.valores,
-            spanGaps: false,
+}
+
+graficar() {
+  this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+    type: 'pie',
+    data: {
+      labels: this.etiquetas,
+      datasets: [{
+        label: this.funcion,
+        data: this.valores,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+          yAxes: [{
+              ticks: {
+                  beginAtZero:true
+                }
+              }]
           }
-        ]
       }
-    });
-  }
+  });
+}
 }
